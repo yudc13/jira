f65261bededc8f368b4f67472c9b77076f94fce1
yudc13 <yudachao13@gmail.com>
2022-01-07 00:11:28 +0800

    text

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,9 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App"><header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
