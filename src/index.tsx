9811e4c2bbc2557002ee5108f321a67cd0a79977
yudc13 <yudachao13@gmail.com>
2022-01-15 23:42:07 +0800

    style: delete index.css

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { loadDevTools } from 'jira-dev-tool';
 import './theme.less';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { AppProvider } from './context';
